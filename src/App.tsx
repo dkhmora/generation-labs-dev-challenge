4f318e975398c2b6c849e06e907cf5b16010e694
Daryl Kiel Mora <dkhmora@gmail.com>
2024-03-07 11:47:04 -0800

    Refactor App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,7 @@ function App() {
       <AppBar />
 
       <div className="flex flex-col px-4 py-6 space-y-16 xl:px-40 lg:flex-row lg:px-20 lg:py-12 lg:space-x-24 lg:space-y-0">
-        <div className="flex flex-1">
-          <ActionPlan userName="Jane Doe" />
-        </div>
-
-        <div className="flex flex-col flex-1 space-y-16"></div>
+        <ActionPlan userName="Jane Doe" />
       </div>
     </main>
   );
